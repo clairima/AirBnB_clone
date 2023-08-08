4cc6a95c6f2e3f96eb5b32e6103dd337d70caf42
Omar Khaled <o.khaled6789@gmail.com>
2023-08-08 23:59:15 +0300

    fixed base_mode, and updated test cases

--- models/base_model.py
+++ models/base_model.py
@@ -20,16 +20,45 @@ class BaseModel:
         is created and it will be updated every time you change your object
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         The constructor of our instances:
         * creates a unique id using uuid.uuid4()
         * initialize created_at with the current time
         * initialize updated_at with the current time
+
+        Args:
+        *args: Unused positional arguments.
+        **kwargs: Keyword arguments used for recreating an instance from
+        dictionary representation.
+
+        If kwargs is not empty:
+        - Recreates instance attributes from the provided dictionary
+            representation.
+        - Converts 'created_at' and 'updated_at' strings to datetime objects.
+        - If 'id' and 'created_at' are missing, generates them.
+        If kwargs is empty:
+        - Creates a new instance with fresh 'id', 'created_at',
+        and 'updated_at'.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.datetime.strptime
+                                (value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.datetime.now()
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self, name='BaseModel'):
         """
@@ -56,40 +85,8 @@ class BaseModel:
         * created_at and updated_at must be converted to string object
             in ISO format
         """
-        temp_dic = self.__dict__
+        temp_dic = self.__dict__.copy()
         temp_dic['created_at'] = temp_dic['created_at'].isoformat()
         temp_dic['updated_at'] = temp_dic['updated_at'].isoformat()
         temp_dic['__class__'] = self.__class__.__name__
-
-return temp_dic
-
-    def __init__(self, *args, **kwargs):
-        """
-        Initializes a new instance of BaseModel.
-
-        Args:
-        *args: Unused positional arguments.
-        **kwargs: Keyword arguments used for recreating an instance from dictionary representation.
-
-        If kwargs is not empty:
-        - Recreates instance attributes from the provided dictionary representation.
-        - Converts 'created_at' and 'updated_at' strings to datetime objects.
-        - If 'id' and 'created_at' are missing, generates them.
-        If kwargs is empty:
-        - Creates a new instance with fresh 'id', 'created_at', and 'updated_at'.
-        """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key == 'created_at' or key == 'updated_at':
-                        setattr(self, key, datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                    else:
-                        setattr(self, key, value)
-            if 'id' not in kwargs:
-                self.id = str(uuid.uuid4())
-            if 'created_at' not in kwargs:
-                self.created_at = datetime.datetime.now()
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+        return temp_dic
